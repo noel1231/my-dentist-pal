07e7f40d533a036808b8a9942ee7b868a26109ac
albertdaracan@gmail.com <albertdaracan@gmail.com@083cbde2-653a-620e-a617-15f130e7f0be>
2013-09-12 08:30:51 +0000

    Contact_Us

--- branches/codeigniter/js/custom.js
+++ branches/codeigniter/js/custom.js
@@ -32,65 +32,65 @@ $( document ).ready(function()
                 onOverlayClick: $.unblockUI 
             });
         }
-        else if(child_html == "Submit")
-        {
-            var fullname = $('#fullname').val();
-            var email = $('#email_address').val();
-            var comments = $('#message').val();
+   });
+   
+   $('#submit').click(function()
+   {
+        var fullname = $('#fullname').val();
+        var email = $('#email_address').val();
+        var comments = $('#message').val();
 
-            var valid = '';
-            
-            if(fullname.length < 1 || email.length < 1 || comments.length < 1)
-            {
-                    valid += 'All fields required!';
-                    $("#error").html("Error: "+valid);
-                    $("#error").show();		
-            }
-            else if(!email.match(/^([a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,4}$)/i)){
-                    valid += 'Please enter a valid email!';
-                    $("#error").html("Error: "+valid);
-                    $("#error").show();
-            }
-            else
-            {
-                $.blockUI
-                ({ css:
-                    { 
-                        border: 'none', 
-                        padding: '15px', 
-                        backgroundColor: '#000', 
-                        '-webkit-border-radius': '10px', 
-                        '-moz-border-radius': '10px', 
-                        opacity: .6, 
-                        color: '#fff' 
-                    } 
-                });
-
-                $.ajax
-                ({
-                    type: 'post',
-                    url: "/send_mail.php",
-                    data:{
-                            'full_name' : fullname,
-                            'email' : email,
-                            'message' : comments
-                         },
-                    cache: false,
-                    success: function(html)
-                    {
-                        $.unblockUI
-                        ({
-                            onUnblock: function()
-                            {
-                            } 
-                        });
-                        
-                        window.location = "/contact_us.php"; 
-                    }
-                  });
-            }
-        }
+        var valid = '';
         
+        if(fullname.length < 1 || email.length < 1 || comments.length < 1)
+        {
+                valid += 'All fields required!';
+                $("#error").html("Error: "+valid);
+                $("#error").show();		
+        }
+        else if(!email.match(/^([a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,4}$)/i)){
+                valid += 'Please enter a valid email!';
+                $("#error").html("Error: "+valid);
+                $("#error").show();
+        }
+        else
+        {
+            $.blockUI
+            ({ css:
+                { 
+                    border: 'none', 
+                    padding: '15px', 
+                    backgroundColor: '#000', 
+                    '-webkit-border-radius': '10px', 
+                    '-moz-border-radius': '10px', 
+                    opacity: .6, 
+                    color: '#fff' 
+                } 
+            });
+
+            $.ajax
+            ({
+                type: 'post',
+                url: "/contact_us/send",
+                data:{
+                        'full_name' : fullname,
+                        'email' : email,
+                        'message' : comments
+                     },
+                cache: false,
+                success: function()
+                {
+                    $.unblockUI
+                    ({
+                        onUnblock: function()
+                        {
+                        } 
+                    });
+                    
+                    window.location = "/contact_us"; 
+                }
+              });
+        }
    });
     
 });
